3bc80dade02e0e205bcb84d0becfe1191ed671ec
Qiusheng Wu <giswqs@gmail.com>
2023-03-18 15:03:47 -0400

    Added mapbox

---
+++ mapwidget/javascript/mapbox.js
@@ -0,0 +1,37 @@
+function loadScript(src) {
+    return new Promise((resolve, reject) => {
+      let script = Object.assign(document.createElement("script"), {
+        type: "text/javascript",
+        async: true,
+        src: src,
+      });
+      script.addEventListener("load", resolve);
+      script.addEventListener("error", reject);
+      document.body.appendChild(script);
+    });
+  };
+  
+  await loadScript("https://api.mapbox.com/mapbox-gl-js/v2.13.0/mapbox-gl.js");
+  
+  export function render(view) {
+
+    let center = view.model.get("center");
+    let zoom = view.model.get("zoom");
+    let width = view.model.get("width");
+    let height = view.model.get("height");
+
+    const div = document.createElement("div");
+    div.style.width = width;
+    div.style.height = height;
+
+    let token = view.model.get("token");
+    
+  mapboxgl.accessToken = token
+  const map = new mapboxgl.Map({
+  container: div,
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: center,
+  zoom: zoom
+  });
+    view.el.appendChild(div);
+  }
---
+++ mapwidget/mapbox.py
@@ -0,0 +1,16 @@
+import os
+import pathlib
+import anywidget
+import traitlets
+
+
+class Map(anywidget.AnyWidget):
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    _esm = pathlib.Path(os.path.join(cwd, 'javascript', 'mapbox.js'))
+    _css = pathlib.Path(os.path.join(cwd, 'styles', 'mapbox.css'))
+    default_token = os.environ.get('MAPBOX_TOKEN')
+    token = traitlets.Unicode(default_token).tag(sync=True)
+    center = traitlets.List([0, 20]).tag(sync=True, o=True)
+    zoom = traitlets.Int(2).tag(sync=True, o=True)
+    width = traitlets.Unicode('100%').tag(sync=True, o=True)
+    height = traitlets.Unicode('300px').tag(sync=True, o=True)
