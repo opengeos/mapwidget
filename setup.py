5fe11dc43fa2a872f7da903fa1cf0fcf8e321101
Qiusheng Wu <giswqs@gmail.com>
2023-03-18 01:23:13 -0400

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/giswqs/mapwidget',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
