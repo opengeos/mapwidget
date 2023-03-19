b83980fad1bebcd9849d7325872ecfa3c92ee076
Qiusheng Wu <giswqs@gmail.com>
2023-03-19 13:41:47 -0400

    Added set_esm() to maplibre

--- mapwidget/javascript/maplibre.js
+++ mapwidget/javascript/maplibre.js
@@ -1,6 +1,7 @@
 import maplibregl from "https://esm.sh/maplibre-gl@2.4.0";
 
 export function render(view) {
+    // Header
     let center = view.model.get("center");
     center.reverse();
     let zoom = view.model.get("zoom");
@@ -11,11 +12,14 @@ export function render(view) {
     div.style.width = width;
     div.style.height = height;
 
+    // Map content
     const map = new maplibregl.Map({
         container: div,
         style: "https://demotiles.maplibre.org/style.json", // stylesheet location
         center: center, // starting position [lng, lat]
         zoom: zoom, // starting zoom
     });
+
+    // Footer
     view.el.appendChild(div);
 }
--- mapwidget/maplibre.py
+++ mapwidget/maplibre.py
@@ -5,10 +5,8 @@ import traitlets
 
 
 class Map(anywidget.AnyWidget):
-    """Map widget
+    """Create a Maplibre map widget.
 
-    Args:
-        anywidget (_type_): _description_
     """
 
     _cwd = os.path.dirname(os.path.abspath(__file__))
@@ -19,11 +17,15 @@ class Map(anywidget.AnyWidget):
     width = traitlets.Unicode('100%').tag(sync=True, o=True)
     height = traitlets.Unicode('600px').tag(sync=True, o=True)
 
-    def set_esm(self, esm):
+    def set_esm(self, esm, container='map'):
         """Set esm attribute. Can be a string, a file path, or a url.
+            See examples at https://maplibre.org/maplibre-gl-js-docs/example/
+            Open an example and click on the 'Edit in CodePen' button.
+            Then copy the code from the 'JS' tab, and assign it to the esm parameter.
 
         Args:
             esm (str): The esm string, file path, or url.
+            container (str, optional): The container name. Defaults to 'map'.
 
         Raises:
             TypeError: If esm is not a string.
@@ -31,20 +33,25 @@ class Map(anywidget.AnyWidget):
         if isinstance(esm, str):
             if os.path.isfile(esm):
                 with open(esm, 'r') as f:
-                    self._esm = f.read()
+                    content = f.read()
             elif esm.startswith('http'):
                 import urllib.request
 
                 with urllib.request.urlopen(esm) as response:
-                    self._esm = response.read().decode('utf-8')
+                    content = response.read().decode('utf-8')
             else:
-                self._esm = esm
+                content = esm
+
+            self._esm = self._create_esm(content, container=container)
+
         else:
             raise TypeError('esm must be a string')
 
-    def set_css(self, css):
+    def set_css(self, css, container='map'):
         """Set css attribute. Can be a string, a file path, or a url.
-
+            See examples at https://maplibre.org/maplibre-gl-js-docs/example/
+            Open an example and click on the 'Edit in CodePen' button.
+            Then copy the code from the 'CSS' tab, and assign it to the css parameter.
         Args:
             css (str): The css string, file path, or url.
 
@@ -54,13 +61,61 @@ class Map(anywidget.AnyWidget):
         if isinstance(css, str):
             if os.path.isfile(css):
                 with open(css, 'r') as f:
-                    self._css = f.read()
+                    content = f.read()
             elif css.startswith('http'):
                 import urllib.request
 
                 with urllib.request.urlopen(css) as response:
-                    self._css = response.read().decode('utf-8')
+                    content = response.read().decode('utf-8')
             else:
-                self._css = css
+                content = css
+
+            self._css = content.replace(f'#{container}', f'#div').replace(
+                f'.{container}', f'.div'
+            )
         else:
             raise TypeError('css must be a string')
+
+    def _create_esm(self, esm, container='map'):
+        """Create esm string by replacing the container name.
+
+        Args:
+            esm (str): The esm string.
+            container (str, optional): The container name. Defaults to 'map'.
+
+        Returns:
+            str: The esm string with the container name replaced.
+        """
+        _cwd = os.path.dirname(os.path.abspath(__file__))
+        _esm = pathlib.Path(os.path.join(_cwd, 'javascript', 'maplibre.js'))
+
+        with open(_esm, 'r') as f:
+            lines = f.readlines()
+
+        header = []
+        footer = []
+
+        for index, line in enumerate(lines):
+            if line.strip() == '// Map content':
+                header = lines[: index + 1]
+                break
+
+        for index, line in enumerate(lines):
+            if line.strip() == '// Footer':
+                footer = lines[index:]
+                break
+
+        content = esm.replace(f"'{container}'", "div").replace(f'"{container}"', 'div')
+        esm = ''.join(header) + content + ''.join(footer)
+
+        return esm
+
+    def _save_esm(self, output):
+        """Save esm to file
+
+        Args:
+            output (str): The output file path.
+        """
+
+        with open(output, 'w') as f:
+            f.write(self._esm)
